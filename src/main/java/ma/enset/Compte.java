825691233b12ea8e7ef1164184c231a015ff29ad
Wahiba275 <123891320+Wahiba275@users.noreply.github.com>
2023-04-27 22:54:24 +0000

    Initial commit

---
+++ src/main/java/ma/enset/Compte.java
@@ -0,0 +1,49 @@
+package ma.enset;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+import java.util.Date;
+
+@XmlRootElement(name = "compte")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Compte {
+  private  int code ;
+  private  double montant ;
+  @XmlTransient
+  private Date dateCreation;
+
+    public Compte() {
+    }
+
+    public Compte(int code, double montant, Date dateCreation) {
+        this.code = code;
+        this.montant = montant;
+        this.dateCreation = dateCreation;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+}
